584ea1c8fee906d4af107cd0df8ce91954a007a8
Joás Martins <joasmcarmo@gmail.com>
2024-11-21 12:03:37 -0300

    Aula 4 (incompleto)

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,17 @@
-import getTodosOsPosts from "../models/postsModel.js";
+import {getTodosOsPosts} from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
     const result = await getTodosOsPosts()
     res.status(200).json(result);
+};
+
+export async function pubPosts(req, res) {
+    const novoPost = req.body;
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message)
+        res.status(500).json({ "Erro:": "Falha na requisição!" })
+    }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,8 +1,14 @@
 import conectarAoBanco from "../config/dbConfig.js";
 const database = await conectarAoBanco(process.env.STRING_CONECTION);
 
-export default async function getTodosOsPosts() {
+export async function getTodosOsPosts() {
     const db = database.db("api-googleai");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = database.db("api-googleai");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost)
 }
